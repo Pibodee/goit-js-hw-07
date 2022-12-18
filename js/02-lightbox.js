660587fcbda27ea51207304705d5c9b7a8c641d5
Pibodee <mrbodyanuch@gmail.com>
2022-12-18 17:35:02 +0200

    added slides with errors

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,18 +4,18 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery')
 
 const markup = galleryItems.map(({ preview, original, description }) => `<a href="${original}" class="gallery__item"><img src="${preview}" data-source = "${original}" alt="${description}" class="gallery__image"></a>`).join('')
-gallery.insertAdjacentHTML('afterbegin', markup)
-// new SimpleLightbox(markup, { /* options */ });
-    new SimpleLightbox('.gallery', { /* options */ });
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+
+const slider = new SimpleLightbox('.gallery__item');
+slider.next()
+
 
 gallery.addEventListener('click', evt => {
     evt.preventDefault();
     if (!evt.target.classList.contains('gallery__image')) {
         return
-    } 
-    const largeUrl = evt.target.dataset.source;
-
-    
+    }        
 })
 
 console.log(galleryItems);
