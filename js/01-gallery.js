3e8b6b777ca39d6ab3f128d4f8a6c02b1063146f
Pibodee <mrbodyanuch@gmail.com>
2022-12-18 14:27:39 +0200

    First task added Esc function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -14,7 +14,19 @@ gallery.addEventListener('click', (evt) => {
     const largeUrl = evt.target.dataset.source;
     const largeImg = basicLightbox.create(`<img src = ${largeUrl} width 800 height 600>`)
     largeImg.show()
+
+    const link = document.querySelectorAll('.gallery__link')
+
+    link.forEach(opened => {
+        if (opened.getAttribute('href') == largeUrl) {
+            opened.addEventListener('keydown', e => {
+                if (e.key === 'Escape') {
+                    largeImg.close()
+                }return
+            })
+        }
+    })
 })
 
-// console.log(galleryItems);
 
+// console.log(galleryItems);
