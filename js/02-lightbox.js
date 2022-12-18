88b6e0b873e02b83b9fc8874bf46cd176bef0fa3
Pibodee <mrbodyanuch@gmail.com>
2022-12-18 18:24:20 +0200

    Task 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,16 +6,14 @@ const gallery = document.querySelector('.gallery')
 const markup = galleryItems.map(({ preview, original, description }) => `<a href="${original}" class="gallery__item"><img src="${preview}" data-source = "${original}" alt="${description}" class="gallery__image"></a>`).join('')
 gallery.insertAdjacentHTML('afterbegin', markup);
 
-
-const slider = new SimpleLightbox('.gallery__item');
-slider.next()
-
-
-gallery.addEventListener('click', evt => {
-    evt.preventDefault();
-    if (!evt.target.classList.contains('gallery__image')) {
-        return
-    }        
+gallery.addEventListener("click", evt => {
+  evt.preventDefault();
 })
+
+const slide = new SimpleLightbox(".gallery a", {
+    captionsData: `alt`,
+    captionDelay: `250`,
+});
+
 
 console.log(galleryItems);
